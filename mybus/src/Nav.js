11f602a27032a7de03595a9cd69fd6a4ae4298b6
Lenkaa3792 <allanlenkaa@gmail.com>
2022-09-07 09:37:21 +0300

    navigation bar components

--- mybus/src/Nav.js
+++ mybus/src/Nav.js
@@ -1,9 +1,26 @@
 import React from "react";
+import {NavLink}from "react-router-dom";
 
-
-function Nav ()
+export default function Nav ()
 {
     return (
+      <Nav>
         
-    )
+        
+          <NavLink to="/about" activeStyle>
+            About
+          </NavLink>
+          <NavLink to="/services" activeStyle>
+            Services
+          </NavLink>
+          <NavLink to="/book" activeStyle>
+            Book
+          </NavLink>
+          <NavLink to="/contact" activeStyle>
+            Contact
+          </NavLink>
+        
+      </Nav>
+    );
+    
 }
