ffe883c674dc81b58855428e7ed5cc8520f18637
Lenkaa372 <allanlenkaa@gmail.com>
2022-09-12 10:17:37 +0300

    styling booking page

--- mybus/src/Book.js
+++ mybus/src/Book.js
@@ -6,30 +6,24 @@ export default function Book(){
 <div id="book">
  <div id="form">
  <Form >
-     <h1>Request for assitance/Booking</h1>
+     <h1>Bus booking form</h1>
   <form action="#">
-    <fieldset>
       <label for="name"> Name:
         <input type="text" name="name" required />
       </label>
       <label for="last-name">Enter Your destination: <input type="text" name="last-name" required /></label>
+      <label for="last-name">Start of journey: <input type="text" name="last-name" required /></label>
       <label for="number">Phone number :
         <input type="number" name="phone"  required /></label>
-    </fieldset>
-    <fieldset>
-      <label for="file">Upload the  car picture: <input type="file" name="file" /></label>
-      <label for="age">location  (probably your exact map geo map):
-        <input /></label>
-      <label for="referrer">How can we help you?
+        <input />
+      <label for="referrer">book return ?
         <select name="need">
-          <option value="">(select one)</option>
-          <option value="1">Tow and repair</option>
-          <option value="2">Facebeat</option>
-          <option value="3">Full body work</option>
-          <option value="4">shocks</option>
+          <option value="">(select)</option>
+          <option value="1">yes</option>
+          <option value="2">no</option>
         </select>
       </label>
-    </fieldset>
+   
     <input type="submit" value="Submit" />
   </form>
  </Form>
