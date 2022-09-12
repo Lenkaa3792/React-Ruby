6a26058734224b65a5ba0d2017922cfff387d7e1
Lenkaa372 <allanlenkaa@gmail.com>
2022-09-12 16:52:27 +0300

    styling  about page

--- mybus/src/About.js
+++ mybus/src/About.js
@@ -1,5 +1,6 @@
 import React from "react";
-import image1 from "./images/bus2.jpeg"
+import image1 from "./images/bus.jpg"
+import image2 from "./images/bus2.jpeg"
 
 
 export default function About(){
@@ -9,8 +10,12 @@ export default function About(){
         
           <div className="p4">
           <p className="p3"> Coast Bus company has established itself as a leader in the travel industry. The luxury bus is one of the best picks if you want to travel to Mombasa. They operate in Kenya, Uganda, Tanzania, and Rwanda.Travelling from any part of Kenya to Mombasa can be hectic and long. However, you can arrive to the city via the various bus companies that are available.</p>
+
           </div>
-         
+         <div className="image2">
+            <img src={image2}  className='imageclass2'/>
+
+         </div>
 
         </div>
     )
