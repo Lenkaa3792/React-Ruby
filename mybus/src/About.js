61cceadb1634b3e7e141be8e90d4b2f22a45a90d
Lenkaa372 <allanlenkaa@gmail.com>
2022-09-12 09:56:53 +0300

    image

--- mybus/src/About.js
+++ mybus/src/About.js
@@ -1,7 +1,12 @@
 import React from "react";
+import {image1} from "./image/bus.jpg"
+
 
 export default function About(){
     return (
-        <div></div>
+        <div>
+            i<img src={image1}/>
+
+        </div>
     )
 }
