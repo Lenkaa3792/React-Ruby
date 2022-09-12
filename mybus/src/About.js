ee6cac0d8b3389dc5e0f50db095a7ce839e4f759
Lenkaa372 <allanlenkaa@gmail.com>
2022-09-12 10:35:36 +0300

    styling about page

--- mybus/src/About.js
+++ mybus/src/About.js
@@ -1,11 +1,11 @@
 import React from "react";
-// import {image1} from "./image/bus.jpg"
+// import {image1} from "/image/bus.jpg"
 
 
 export default function About(){
     return (
         <div>
-            {/* i<img src={image1}/> */}
+          <p className="p1">Modern Coast Bus company has established itself as a leader in the travel industry. The luxury bus is one of the best picks if you want to travel to Mombasa. They operate in Kenya, Uganda, Tanzania, and Rwanda.Travelling from any part of Kenya to Mombasa can be hectic and long. However, you can arrive to the city via the various bus companies that are available.</p>
 
         </div>
     )
